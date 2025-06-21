d3a403653ce7437beaab7984bbaec7756e592c79
rediskaSgryadki <zzzsashapirka@gmail.com>
2025-06-22 02:00:21 +1000

    Update views.py

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .mailgun import send_feedback_email
+from .notisend import send_feedback_email
 from rest_framework.permissions import AllowAny
 
 class FeedbackView(APIView):

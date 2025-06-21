2b9150c245628e5247e327662582654434ac319b
rediskaSgryadki <zzzsashapirka@gmail.com>
2025-06-22 02:42:40 +1000

    Update urls.py

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FeedbackView
 
 urlpatterns = [
-    path('', FeedbackView.as_view(), name='feedback'),
+    path('feedback/', FeedbackView.as_view(), name='feedback'),
 ] 

241d62e108226238fa7dd0f66ba044173322f375
rediskaSgryadki <zzzsashapirka@gmail.com>
2025-06-22 02:42:59 +1000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -165,9 +165,9 @@ AUTH_USER_MODEL = 'users.User'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'rediskaSgryadki@yandex.ru'
+EMAIL_HOST_PASSWORD = 'rediskaSgryadki'
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
